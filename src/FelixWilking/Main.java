83872d98c167a33da50296e5b21630598a2e4f31
fwilking4736 <fwilking4736@bths.edu>
2018-10-16 18:28:45 -0700

    :)

--- src/FelixWilking/Main.java
+++ src/FelixWilking/Main.java
@@ -6,22 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int guess = 50;
-        int guess2 = 100;
-        System.out.println("Is your secret number"+guess+"?");
+        int min = 0;
+        int max = 100;
+        System.out.println("Is your secret number"+(max+min)/2+"?");
         String answer = input.nextLine();
-        for(int i = 0;!(answer.equals("="));i++){
-            if(i%2 == 0) {
-                if (answer.equals("+")) {
-                    guess2 = (guess - guess2)/2 + guess2;
-                } else if (answer.equals("-")) {
-                    guess2 = (guess - guess2)/2 - guess2;
-                }
+        while(!(answer.equals("="))) {
+            if (answer.equals("+")) {
+                min = (max + min) / 2;
+                System.out.println("Is your secret number" + (max + min) / 2 + "?");
+            } else if (answer.equals("-")) {
+                max = (max + min) / 2;
+                System.out.println("Is your secret number" + (max + min) / 2 + "?");
             }
-            else{
-
-            }
-            System.out.println("Is your secret number"+guess+"?");
             answer = input.nextLine();
         }
     }
